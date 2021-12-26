2960d26bb4d982b391c64f769198306c8f2c960a
Alireza Mousavi <76426674+AlirezaMsv@users.noreply.github.com>
2021-12-27 00:21:30 +0330

    Update Faz2.java

--- Faz2/Faz2.java
+++ Faz2/Faz2.java
@@ -1,4 +1,7 @@
+package com.company.sample;
+
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,6 +11,7 @@ public class Faz2 {
     static FileOutputStream fout;
     static int i=0;
     static String line;
+    static int lineNumber =0;
     public static void main(String[] args) throws IOException {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setCurrentDirectory(new File("C:\\Users\\Admin\\Desktop"));
@@ -27,10 +31,17 @@ public class Faz2 {
         ////////////////////////// lexical analysis
         while (sc.hasNextLine()){
             line = sc.nextLine();
+            lineNumber++;
             ws();
             String others = "(){}[], \t\n;";
             StringBuilder st = new StringBuilder();
-            while (others.contains(line.charAt(i)+"")){
+            if (i<line.length()&&others.contains(line.charAt(i)+""))
+                if(isPunc(line.charAt(i)))
+                    fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+            if (i>=line.length()){
+                i=0;
+                continue;}
+            while (i<line.length()&&!others.contains(line.charAt(i)+"")){
                 st.append(line.charAt(i));
                 i++;
             }
@@ -54,10 +65,35 @@ public class Faz2 {
                     returnStmt();
                     break;
                 case "int":
+                    fout.write((lineNumber+"keyword :: int"+"\n").getBytes());
+                    if (line.contains("("))
+                        funcStmt();
+                    else
+                        decStmt();
+                    break;
                 case "float":
+                    fout.write((lineNumber+"keyword :: float"+"\n").getBytes());
+                    if (line.contains("("))
+                        funcStmt();
+                    else
+                        decStmt();
+                    break;
                 case "double":
+                    fout.write((lineNumber+"keyword :: double"+"\n").getBytes());
+                    if (line.contains("("))
+                        funcStmt();
+                    else
+                        decStmt();
+                    break;
                 case "long":
+                    fout.write((lineNumber+"keyword :: long"+"\n").getBytes());
+                    if (line.contains("("))
+                        funcStmt();
+                    else
+                        decStmt();
+                    break;
                 case "bool":
+                    fout.write((lineNumber+"keyword :: bool"+"\n").getBytes());
                     if (line.contains("("))
                         funcStmt();
                     else
@@ -66,174 +102,301 @@ public class Faz2 {
                 default:
                     idStmt(st.toString());
             }
+            i=0;
         }
         ////////////////////////////////
+        System.out.println("faz2 is done");
+        Desktop desktop = Desktop.getDesktop();
+        desktop.open(output);
     }
     // 8
     static void ws(){
-        while(line.charAt(i) == ' ')
+        while(i<line.length()&&line.charAt(i) == ' ')
             i++;
-        while(line.charAt(i) == '\t')
+        while(i<line.length()&&line.charAt(i) == '\t')
             i+=8;
     }
     // 1
     static void ifStmt() throws IOException {
-        fout.write("keyword :: if".getBytes());
+        fout.write((lineNumber+"keyword :: if\n").getBytes());
         ws();
         if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
         ws();
         exprStmt();
         ws();
         if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
         ws();
         if (line.charAt(i)!='\n')
             if(isPunc(line.charAt(i)))
-                fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+                fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
     }
     // 9
     static void elseStmt() throws IOException {
-        fout.write("keyword :: else".getBytes());
+        ws();
+        fout.write((lineNumber+"keyword :: else"+"\n").getBytes());
+        ws();
         if (line.charAt(i)!='\n')
             if(isPunc(line.charAt(i)))
-                fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+                fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
     }
     // 7
     static void forStmt()throws IOException{
-        fout.write("keyword :: for".getBytes());
+        fout.write((lineNumber+"keyword :: for"+"\n").getBytes());
         ws();
-        if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+        if(i<line.length()&&isPunc(line.charAt(i)))
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
         ws();
         decStmt();
-        if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+        ws();
+        if(i<line.length()&&isPunc(line.charAt(i)))
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
         exprStmt();
-        if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+        ws();
+        if(i<line.length()&&isPunc(line.charAt(i)))
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
         operationStmt();
-        if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
-        if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+        ws();
+        if(i<line.length()&&isPunc(line.charAt(i)))
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
+        if(i<line.length()&&isPunc(line.charAt(i)))
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
     }
     // 10
     static void whileStmt()throws IOException{
-        fout.write("keyword :: while".getBytes());
+        ws();
+        fout.write((lineNumber+"keyword :: while"+"\n").getBytes());
+        ws();
         if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
         exprStmt();
+        ws();
         if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
         if(isPunc(line.charAt(i)))
-            fout.write(("punctuation :: "+line.charAt(i++)).getBytes());
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
     }
     // 2
     static void relOpStmt()throws IOException{
+        ws();
         StringBuilder st = new StringBuilder();
         st.append(line.charAt(i++));
         ws();
         if (line.charAt(i)=='=') {
             st.append("=");
             i++;
+            ws();
         }
         switch (st.toString()){
             case ">":
-                fout.write(("relOp :: >").getBytes());
+                fout.write((lineNumber+"relOp :: >"+"\n").getBytes());
+                ws();
                 break;
             case "<":
-                fout.write(("relOp :: <").getBytes());
+                fout.write((lineNumber+"relOp :: <"+"\n").getBytes());
+                ws();
                 break;
             case "!=":
-                fout.write(("relOp :: !=").getBytes());
+                fout.write((lineNumber+"relOp :: !="+"\n").getBytes());
+                ws();
                 break;
             case "==":
-                fout.write(("relOp :: ==").getBytes());
+                fout.write((lineNumber+"relOp :: =="+"\n").getBytes());
+                ws();
                 break;
             case ">=":
-                fout.write(("relOp :: >=").getBytes());
+                fout.write((lineNumber+"relOp :: >="+"\n").getBytes());
+                ws();
                 break;
             case "<=":
-                fout.write(("relOp :: <=").getBytes());
+                fout.write((lineNumber+"relOp :: <="+"\n").getBytes());
+                ws();
                 break;
         }
+        ws();
     }
     // 4
-    static void TermStmt()throws IOException{
-        StringBuilder st = new StringBuilder();
-        String others = "(){}[], \t\n;";
-        int index=i;
-        while (others.contains(line.charAt(index)+"")){
-            st.append(line.charAt(index));
-            index++;
-        }
-        idStmt(st.toString());
-        i=index+1;
-        numberStmt();
+    static void termStmt()throws IOException{
         valueStmt();
+        ws();
     }
     // 5
     static void idStmt(String name)throws IOException{
-        fout.write(("identifier :: "+ name).getBytes());
+        ws();
+        fout.write((lineNumber+"identifier :: "+ name+"\n").getBytes());
+        ws();
+    }
+    static void idStmt()throws IOException{
+        ws();
+        if (!((((int)line.charAt(i))>=65 &&((int)line.charAt(i))<=90) || (((int)line.charAt(i))>=97 &&((int)line.charAt(i))<=122)))
+            return;
+        System.out.println(lineNumber + "  :  " +line.charAt(i));
+        StringBuilder st = new StringBuilder();
+        String others = "(){}, \t\n;-=+*-/%";
+        int index=i;
+        while (!others.contains(line.charAt(index)+""))
+            st.append(line.charAt(index++));
+        if (st.toString().contains("[")){
+            StringBuilder stringBuilder = new StringBuilder();
+            ws();
+            while (st.toString().charAt(i)!='['){
+                stringBuilder.append(st.toString().charAt(i++));
+            }
+            ws();
+            idStmt(stringBuilder.toString());
+            fout.write((lineNumber+"punctuation :: ["+"\n").getBytes());
+            ws();
+            stringBuilder = new StringBuilder();
+            boolean flag=false;
+            while (st.toString().charAt(i)!=']'){
+                stringBuilder.append(st.toString().charAt(i));
+                flag=true;
+                ws();
+            }
+            ws();
+            if (flag)
+                fout.write((lineNumber+"number :: "+stringBuilder+"\n").getBytes());
+            ws();
+            fout.write((lineNumber+"punctuation :: ]"+"\n").getBytes());
+            ws();
+        }
+        else
+            fout.write((lineNumber+"identifier :: "+st.toString()+"\n").getBytes());
+        i=index;
     }
     // 6
     static void valueStmt()throws IOException{
+        ws();
         if (((int)line.charAt(i))>=48 &&((int)line.charAt(i))<=57){
             numberStmt();
         }
         else{
+            ws();
             StringBuilder st = new StringBuilder();
             String others = "(){}[], \t\n;";
             int index=i;
+            ws();
             while (others.contains(line.charAt(index)+"")){
                 st.append(line.charAt(index));
                 index++;
             }
+            ws();
             if (st.toString().equals("true"))
-                fout.write(("boolean :: true").getBytes());
+                fout.write((lineNumber+"boolean :: true"+"\n").getBytes());
             if (st.toString().equals("false"))
-                fout.write(("boolean :: false").getBytes());
-            else
+                fout.write((lineNumber+"boolean :: false"+"\n").getBytes());
+            else if (!others.contains(st.toString()))
                 idStmt(st.toString());
             i=index+1;
+            ws();
         }
         ws();
-        if(isOp(line.charAt(i))){
-
+        while (isOp(line.charAt(i))){
+            idStmt();
+            numberStmt();
         }
+        ws();
     }
     // 14
     static void exprStmt()throws IOException{
-
+        ws();
+        termStmt();
+        ws();
+        relOpStmt();
+        ws();
+        termStmt();
+        ws();
     }
     // 13
     static void operationStmt()throws IOException{
-
+        ws();
+        idStmt();
+        ws();
+        fout.write((lineNumber+"operator :: ="+"\n").getBytes());
+        ws();
+        valueStmt();
+        ws();
+        fout.write((lineNumber+"punctuation :: ;"+"\n").getBytes());
+        ws();
     }
     // 12
     static void returnStmt()throws IOException{
-
+        ws();
+        fout.write((lineNumber+"keyword :: return"+"\n").getBytes());
+        ws();
+        valueStmt();
+        ws();
+        fout.write((lineNumber+"punctuation :: ;"+"\n").getBytes());
+        ws();
     }
     // 11
     static void funcStmt()throws IOException{
-
+        ws();
+        idStmt();
+        ws();
+        if (i<line.length()&&isPunc(line.charAt(i))){
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+            ws();
+        }
+        while(i<line.length()&&!isPunc(line.charAt(i)))
+            decStmt();
+        ws();
+        if (i<line.length()&&isPunc(line.charAt(i))){
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+            ws();
+        }
+        if (i<line.length()&&isPunc(line.charAt(i))){
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+            ws();
+        }
     }
     // 15
     static void decStmt()throws IOException{
-
+        ws();
+        idStmt();
+        ws();
+        if (i<line.length()&&line.charAt(i)=='=') {
+            valueStmt();
+            ws();
+        }
+        if (i<line.length()&&isPunc(line.charAt(i)))
+            fout.write((lineNumber+"punctuation :: "+line.charAt(i++)+"\n").getBytes());
+        ws();
     }
     // 3
     static void numberStmt()throws IOException{
-
+        ws();
+        if (!(((int)line.charAt(i))>=48 &&((int)line.charAt(i))<=57))
+            return;
+        StringBuilder stringBuilder = new StringBuilder();
+        if ((line.charAt(i)+line.charAt(i+1)+"").equals("0x") || (line.charAt(i)+line.charAt(i+1)+"").equals("0b"))
+            stringBuilder.append('0').append(line.charAt(i++));
+        ws();
+        String other = "{}[] \t\n;,";
+        while (!other.contains(line.charAt(i)+"")){
+            stringBuilder.append(line.charAt(i++));
+            ws();
+        }
+        fout.write((lineNumber+"number :: "+stringBuilder+"\n").getBytes());
+        ws();
     }
     //
     static boolean isPunc(char in){
-        String others = "(){}[]";
+        String others = "(){}[];";
         return others.contains(in+"");
     }
     static boolean isOp(char in){
-        String others = "+-*/";
+        String others = "+-*/%";
         return others.contains(in+"");
     }
 }
